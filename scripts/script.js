3cfef210868019ba081ef689c1df0a85d6b879e9
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2022-11-05 18:49:40 -0700

    Added an example of event listener to the button

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
 function sayHello() {
-    
+    console.log("Hello User!");
 }
 //sayHello();
+
+document.getElementById("btn").onclick = function () {
+    location.href = "https://www.youtube.com/";
+};
