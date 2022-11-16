adee7b85cd9e4d8d20728ed81fa4dc681eb51f0c
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2022-11-15 17:09:16 -0800

    succesful listTitle write function

--- scripts/newList.js
+++ scripts/newList.js
@@ -1,32 +1,11 @@
 var currentUser;
 
-function insertName() {
-    firebase.auth().onAuthStateChanged((user) => {
-      // Check if a user is signed in:
-      if (user) {
-        
-        // Do something for the currently logged-in user here:
-        console.log(user.uid);
-        console.log(user.displayName);
-        user_Name = user.displayName;
-  
-        //method #1:  insert with html only
-        //document.getElementById("name-goes-here").innerText = fullName;    //using javascript
-        // document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-        //method #2:  insert using jquery
-        $("#name-goes-here").text(user_Name); //using jquery
-      } else {
-        // No user is signed in.
-      }
-    });
-  }
-  insertName(); //run the function
 
   function saveNewList() {
 
         let listTitle = document.getElementById('list-title').value;
-        let listItem = document.getElementById('list-item').value;
-        console.log(listTitle, listItem);
+        // let listItem = document.getElementById('list-item').value;
+        console.log(listTitle);
 
         firebase.auth().onAuthStateChanged((user) => {
           // Check if a user is signed in:
@@ -36,7 +15,7 @@ function insertName() {
 
             currentUser.get()
               .then(userDoc => {
-                db.collection("lists").add({
+                db.collection("users").doc(user.uid).collection("lists").add({
                   title: listTitle,
                   timestamp: firebase.firestore.FieldValue.serverTimestamp()
                 }).then(()=> {
@@ -47,48 +26,3 @@ function insertName() {
     })
   }
 
-  function insertTitle() {
-    firebase.auth().onAuthStateChanged((user) => {
-      // Check if a user is signed in:
-      if (user) {
-
-        // Do something for the currently logged-in user here:
-        console.log(user.uid);
-        console.log(user.displayName);
-        user_Name = user.displayName;
-
-    db.collection("users").doc(user.uid).collection("lists")
-    .get()
-    .then(function (snap) {
-      snap.forEach(function(doc) {
-        console.log(doc.data().title)
-        document.getElementById("listTitle1").innerText = doc.data().title;
-      })
-    })
-    }
-})
-}
-insertTitle();
-
-function insertTask() {
-  firebase.auth().onAuthStateChanged((user) => {
-    // Check if a user is signed in:
-    if (user) {
-
-      // Do something for the currently logged-in user here:
-      console.log(user.uid);
-      console.log(user.displayName);
-      user_Name = user.displayName;
-
-  db.collection("users").doc(user.uid).collection("lists").doc("list1").collection("tasks")
-  .get()
-  .then(function (snap) {
-    snap.forEach(function(doc) {
-      console.log(doc.data().details)
-      document.getElementById("listTask1").innerText = doc.data().details;
-    })
-  })
-  }
-})
-}
-insertTask();
