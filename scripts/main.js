3d5831959298d6657ca4a4cf5b1feb34adba706b
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2022-11-15 18:43:34 -0800

    added the function to display the cards

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,9 @@ function insertName() {
   insertName(); //run the function
 
   function insertCards() {
+    let card = document.getElementById("cards");
+    let listDiv = document.getElementById("list");
+
     firebase.auth().onAuthStateChanged((user) => {
       // Check if a user is signed in:
       if (user) {
@@ -34,57 +37,82 @@ function insertName() {
     .get()
     .then(function (snap) {
       snap.forEach(function(doc) {
+        let currentListID = doc.id;
         console.log(doc.data().title)
-        document.getElementById("listTitle1").innerText = doc.data().title;
+        // document.getElementsByClassName("listTitle1").innerText = doc.data().title;
+        var listTitle = doc.data().title;
+
+        let testCard = card.content.cloneNode(true);
+        testCard.querySelector('.card-title').innerHTML = listTitle;
+        // loop for adding in tasks items  
+        db.collection("users").doc(user.uid).collection("lists").doc(currentListID).collection("tasks")
+        .get()
+        .then(function (snap2) {
+          let taskItem = document.getElementById("taskItem");
+          let taskList = document.getElementById("card-body");
+
+          snap2.forEach(function(doc) {
+            console.log(doc.data().details)
+
+            var taskDetails = doc.data().details;
+
+            let testTask = taskItem.content.cloneNode(true);
+            testTask.querySelector('.taskDetails').innerHTML = taskDetails;
+            taskList.appendChild(testTask);
+          })
+        }
+        )
+        listDiv.appendChild(testCard);
       })
     })
     }
 })
 }
 
+insertCards();
 
-  function insertTitle() {
-    firebase.auth().onAuthStateChanged((user) => {
-      // Check if a user is signed in:
-      if (user) {
+//   function insertTitle() {
+//     firebase.auth().onAuthStateChanged((user) => {
+//       // Check if a user is signed in:
+//       if (user) {
 
-        // Do something for the currently logged-in user here:
-        console.log(user.uid);
-        console.log(user.displayName);
-        user_Name = user.displayName;
+//         // Do something for the currently logged-in user here:
+//         console.log(user.uid);
+//         console.log(user.displayName);
+//         user_Name = user.displayName;
 
-    db.collection("users").doc(user.uid).collection("lists")
-    .get()
-    .then(function (snap) {
-      snap.forEach(function(doc) {
-        console.log(doc.data().title)
-        document.getElementById("listTitle1").innerText = doc.data().title;
-      })
-    })
-    }
-})
-}
-insertTitle();
+//     db.collection("users").doc(user.uid).collection("lists")
+//     .get()
+//     .then(function (snap) {
+//       snap.forEach(function(doc) {
+//         console.log(doc.data().title)
+//         document.getElementById("listTitle1").innerText = doc.data().title;
+//       })
+//     })
+//     }
+// })
+// }
+// insertTitle();
 
-function insertTask() {
-  firebase.auth().onAuthStateChanged((user) => {
-    // Check if a user is signed in:
-    if (user) {
+// function insertTask() {
+//   firebase.auth().onAuthStateChanged((user) => {
+//     // Check if a user is signed in:
+//     if (user) {
 
-      // Do something for the currently logged-in user here:
-      console.log(user.uid);
-      console.log(user.displayName);
-      user_Name = user.displayName;
+//       // Do something for the currently logged-in user here:
+//       console.log(user.uid);
+//       console.log(user.displayName);
+//       user_Name = user.displayName;
 
-  db.collection("users").doc(user.uid).collection("lists").doc("list1").collection("tasks")
-  .get()
-  .then(function (snap) {
-    snap.forEach(function(doc) {
-      console.log(doc.data().details)
-      document.getElementById("listTask1").innerText = doc.data().details;
-    })
-  })
-  }
-})
-}
-insertTask();
+//   db.collection("users").doc(user.uid).collection("lists").doc("list1").collection("tasks")
+//   .get()
+//   .then(function (snap) {
+//     snap.forEach(function(doc) {
+//       console.log(doc.data().details)
+//       document.getElementById("listTask1").innerText = doc.data().details;
+//     })
+//   })
+//   }
+// })
+// }
+// insertTask();
