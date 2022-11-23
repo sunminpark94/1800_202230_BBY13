f346b3b589ad3f558b77cb7cf43f46ad723e9a5b
Abhishek Chouhan <abhishekchouhannk@gmail.com>
2022-11-22 22:23:31 -0800

    Added in functions to append new task item in new list

--- scripts/newList.js
+++ scripts/newList.js
@@ -11,10 +11,10 @@ function insertName() {
       user_Name = user.displayName;
 
       //method #1:  insert with html only
-      //document.getElementById("name-goes-here").innerText = fullName;    //using javascript
-      // document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
+      // document.getElementById("name-goes-here").innerText = fullName;    //using javascript
+      document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
       //method #2:  insert using jquery
-      $("#name-goes-here").text(user_Name); //using jquery
+      // $("#name-goes-here").text(user_Name); //using jquery
     } else {
       // No user is signed in.
     }
@@ -47,4 +47,39 @@ insertName(); //run the function
       }
     })
   }
+
+// const listItems = document.querySelectorAll('.list-item');
+// x = 0;
+// listItems.forEach(listitem => {
+//   listitem.addEventListener('click', function handleClick(event) {
+//     console.log('listitem clicked', event);
+//     x += 1;
+//     addNewTask();
+//   });
+// });
+document.querySelector('.list-item').addEventListener('keypress' , function handlePress(event) {
+  if (event.key === 'Enter') {
+    addNewTask();
+  }
+
+})
+
+let noOfTasks = 1;
+function addNewTask() {
+
+      // Create a clone of element with id t0:
+let clone = document.querySelector('#t0').cloneNode( true );
+
+// Change the id attribute of the newly created element:
+clone.setAttribute( 'id', "t" + noOfTasks);
+noOfTasks++;
+console.log(noOfTasks);
+clone.addEventListener('keypress' , function handlePress(event) {
+  if (event.key === 'Enter') {
+    addNewTask();
+  }
+})
+// Append the newly created element on element taskItems 
+document.querySelector('#taskItems').appendChild( clone );
+}
 
