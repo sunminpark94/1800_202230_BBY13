e1fafe97b1297c9581f9c258021c9f3a72f4d802
Mina Park <spark264@my.bcit.ca>
2022-11-27 17:36:26 -0800

    styled index.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded',function(event){
   // array with texts to type in typewriter
-  var messages = ["\u2713 TA-DA"];
+  var messages = ["\u2713 TA-DA!"];
 
   function typeWriter(text, index , messageIndex) {
       if (index < text.length) {
           document.querySelector("#intro").innerHTML = text.substring(0, index+1) +'<span aria-hidden="true"></span>';
           setTimeout(function() {
               typeWriter(text, index + 1, messageIndex);
-          }, 150);
+          }, 160);
       } else {
           setTimeout(function() {
               iterateThroughMessages(messageIndex + 1);
